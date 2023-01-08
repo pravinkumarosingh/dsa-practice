9f3b5df023cc4fbb6408890b487b5a9b986d3df6
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2023-01-08 17:36:12 +0530

    recursion example 1 added.

---
+++ 02_recursion/Example1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void func(int n)
+{
+    if (n > 0)
+    {
+        printf("%d\n", n);
+        func(n - 1);
+    }
+}
+
+int main()
+{
+    func(3);
+    return 0;
+}
