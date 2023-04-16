92064309e119a6ccb8ae79b3f55a79b851a68dcc
The Sugar Glider <thesugarglider7@gmail.com>
2023-04-16 17:49:14 +0530

    initial array example

--- 03_arrays/Example1.cpp
+++ 03_arrays/Example1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    int a[5];
-    int b[5] = {2,4,6,8,10};
-    int c[5] = {2,4};
-    int d[5] = {0};
-    int e[]  = {2,4,6,8,10,12};
+    int a[5];                   //garbage value
+    int b[5] = {2,4,6,8,10};    //assigned value
+    int c[5] = {2,4};           //element 1 and element 2 initialised rest elements are 0.
+    int d[5] = {0};             //all the elements are 0.
+    int e[]  = {2,4,6,8,10,12}; //array of size 6.
 
     for (int i=0;i<5;i++){
         cout << a[i] << " ";
